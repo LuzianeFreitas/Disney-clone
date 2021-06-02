3fbafeb8c43afdb893cf763816678895fc7aff87
Luziane Freitas <freitas.lu@outlook.com>
2021-06-02 10:45:38 -0300

    add: carousel

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     // margin:10,
-//     nav:true,
-//     // responsive:{
-//     //     0:{
-//     //         items:1
-//     //     },
-//     //     600:{
-//     //         items:3
-//     //     },
-//     //     1000:{
-//     //         items:5
-//     //     }
-//     // }
-// })
+$(document).ready(function(){
+  $('.main-carousel').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
+
